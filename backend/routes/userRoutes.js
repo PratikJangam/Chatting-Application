da869c8a31cc613fbbccdc1f1e013d624d0a5810
Pratik Jangam <pratikjangam57@gmail.com>
2022-03-27 23:11:37 +0530

    Successfull mongodb Connection

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+// router.route("/").post(registreUser);
+
+// router.post("/login", authUser);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const chats = require("./data/data");
 const dotenv = require("dotenv");
-const app = express();
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
+connectDB();
+const app = express();
 
 app.get("/", () => {
   console.log("App is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("App is listening on port 5000...");
