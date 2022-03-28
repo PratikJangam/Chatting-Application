4841a78a0d1472d4c5a91723e4e5713697ac0d5e
Pratik Jangam <pratikjangam57@gmail.com>
2022-03-28 10:42:04 +0530

    Search User

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -56,4 +56,21 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+const getAllUser = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  // console.log(keyword);
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, getAllUser };
